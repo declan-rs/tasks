fdca335395aecb5de09b6f11d0719a3f0e20eca6
declan-rs <declan.rsg@gmail.com>
2026-08-08 20:48:07 +1000

    Generate next task

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ class List {
 const list = new List("List", ["read emails", "write report", "tidy desk"]);
 displayList(list);
 
+const generateButton = document.createElement("button");
+generateButton.innerText = "Generate next task";
+generateButton.addEventListener("click", () => {generateNextItem(list)});
+document.body.appendChild(generateButton);
+
 function displayList(list) {
     const container = document.getElementById("list");
+    container.replaceChildren();
 
     const title = document.createElement("h3");
     title.textContent = list.title;
@@ -24,4 +30,14 @@ function displayList(list) {
 
     container.appendChild(title);
     container.appendChild(ls);
+}
+
+function generateNextItem(list) {
+    const nextNum = Math.floor(Math.random() * list.items.length);
+    const nextTask = document.createElement("text");
+    nextTask.textContent = "Next task: " + list.items[nextNum] + "";
+    document.body.append(nextTask);
+
+    list.items.splice(nextNum, 1);
+    displayList(list);
 }
